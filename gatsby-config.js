f82f85d941d344a1b72962aabce618568267e085
Takuya Tejima <tejitak@gmail.com>
2020-08-02 13:06:16 +0800

    use yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,135 +141,135 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-catch-links`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: config.manifestName,
-    //     short_name: config.manifestShortName,
-    //     start_url: config.manifestStartUrl,
-    //     background_color: config.manifestBackgroundColor,
-    //     theme_color: config.manifestThemeColor,
-    //     display: config.manifestDisplay,
-    //     icons: [
-    //       {
-    //         src: "/icons/icon-48x48.png",
-    //         sizes: "48x48",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/icons/icon-96x96.png",
-    //         sizes: "96x96",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/icons/icon-144x144.png",
-    //         sizes: "144x144",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/icons/icon-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/icons/icon-256x256.png",
-    //         sizes: "256x256",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/icons/icon-384x384.png",
-    //         sizes: "384x384",
-    //         type: "image/png"
-    //       },
-    //       {
-    //         src: "/icons/icon-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png"
-    //       }
-    //     ]
-    //   }
-    // },
-    // `gatsby-plugin-offline`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `UA-107745192-2`
-    //   }
-    // },
-    // {
-    //   resolve: `gatsby-plugin-feed`,
-    //   options: {
-    //     query: `
-    //       {
-    //         site {
-    //           siteMetadata {
-    //             title
-    //             description
-    //             siteUrl
-    //             site_url: siteUrl
-    //           }
-    //         }
-    //       }
-    //     `,
-    //     feeds: [
-    //       {
-    //         serialize: ({ query: { site, allMarkdownRemark } }) => {
-    //           return allMarkdownRemark.edges.map(edge => {
-    //             return Object.assign({}, edge.node.frontmatter, {
-    //               description: edge.node.excerpt,
-    //               date: edge.node.fields.prefix,
-    //               url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-    //               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-    //               custom_elements: [{ "content:encoded": edge.node.html }]
-    //             });
-    //           });
-    //         },
-    //         query: `
-    //           {
-    //             allMarkdownRemark(
-    //               limit: 1000,
-    //               sort: { order: DESC, fields: [fields___prefix] },
-    //               filter: {
-    //                 fields: {
-    //                   prefix: { ne: null },
-    //                   slug: { ne: null }
-    //                 },
-    //                 frontmatter: {
-    //                   author: { ne: null }
-    //                 }
-    //               }
-    //             ) {
-    //               edges {
-    //                 node {
-    //                   excerpt
-    //                   html
-    //                   fields {
-    //                     slug
-    //                     prefix
-    //                   }
-    //                   frontmatter {
-    //                     title
-    //                   }
-    //                 }
-    //               }
-    //             }
-    //           }
-    //         `,
-    //         output: "/rss.xml"
-    //       }
-    //     ]
-    //   }
-    // },
-    // {
-    //   resolve: `gatsby-plugin-sitemap`
-    // },
-    // {
-    //   resolve: "gatsby-plugin-react-svg",
-    //   options: {
-    //     include: /svg-icons/
-    //   }
-    // },
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icons: [
+          {
+            src: "/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-107745192-2`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.fields.prefix,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [fields___prefix] },
+                  filter: {
+                    fields: {
+                      prefix: { ne: null },
+                      slug: { ne: null }
+                    },
+                    frontmatter: {
+                      author: { ne: null }
+                    }
+                  }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields {
+                        slug
+                        prefix
+                      }
+                      frontmatter {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml"
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        include: /svg-icons/
+      }
+    },
     `gatsby-plugin-netlify-cms`
   ]
 };
