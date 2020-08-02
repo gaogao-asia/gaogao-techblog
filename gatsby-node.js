90bb345f0f4c9cc81b216f3db91670d9d6137917
Takuya Tejima <tejitak@gmail.com>
2020-08-02 12:48:35 +0800

    skip createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,115 +35,105 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 };
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-
-  const postTemplate = path.resolve("./src/templates/PostTemplate.js");
-  const pageTemplate = path.resolve("./src/templates/PageTemplate.js");
-  const categoryTemplate = path.resolve("./src/templates/CategoryTemplate.js");
-
-  // Do not create draft post files in production.
-  let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development";
-  console.log(`Using environment config: '${activeEnv}'`);
-  let filters = `filter: { fields: { slug: { ne: null } } }`;
-  if (activeEnv == "production") {
-    filters = `filter: { fields: { slug: { ne: null } , prefix: { ne: null } } }`;
-  }
-  const result = await graphql(
-    `
-        {
-          allMarkdownRemark(
-            ` +
-      filters +
-      `
-            sort: { fields: [fields___prefix], order: DESC }
-            limit: 1000
-          ) {
-            edges {
-              node {
-                id
-                fields {
-                  slug
-                  prefix
-                  source
-                }
-                frontmatter {
-                  title
-                  category
-                }
-              }
-            }
-          }
-        }
-      `
-  );
-  if (result.errors) {
-    console.log(result.errors);
-    throw new Error(result.errors);
-  }
-
-  const items = result.data.allMarkdownRemark.edges;
-
-  // Create category list
-  const categorySet = new Set();
-  items.forEach(edge => {
-    const {
-      node: {
-        frontmatter: { category }
-      }
-    } = edge;
-
-    if (category && category !== null) {
-      categorySet.add(category);
-    }
-  });
-
-  // Create category pages
-  const categoryList = Array.from(categorySet);
-  categoryList.forEach(category => {
-    createPage({
-      path: `/category/${_.kebabCase(category)}/`,
-      component: categoryTemplate,
-      context: {
-        category
-      }
-    });
-  });
-
-  // Create posts
-  const posts = items.filter(item => item.node.fields.source === "posts");
-  posts.forEach(({ node }, index) => {
-    const slug = node.fields.slug;
-    const next = index === 0 ? undefined : posts[index - 1].node;
-    const prev = index === posts.length - 1 ? undefined : posts[index + 1].node;
-    const source = node.fields.source;
-
-    createPage({
-      path: slug,
-      component: postTemplate,
-      context: {
-        slug,
-        prev,
-        next,
-        source
-      }
-    });
-  });
-
-  // and pages.
-  const pages = items.filter(item => item.node.fields.source === "pages");
-  pages.forEach(({ node }) => {
-    const slug = node.fields.slug;
-    const source = node.fields.source;
-
-    createPage({
-      path: slug,
-      component: pageTemplate,
-      context: {
-        slug,
-        source
-      }
-    });
-  });
+  // const { createPage } = actions;
+  // const postTemplate = path.resolve("./src/templates/PostTemplate.js");
+  // const pageTemplate = path.resolve("./src/templates/PageTemplate.js");
+  // const categoryTemplate = path.resolve("./src/templates/CategoryTemplate.js");
+  // // Do not create draft post files in production.
+  // let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development";
+  // console.log(`Using environment config: '${activeEnv}'`);
+  // let filters = `filter: { fields: { slug: { ne: null } } }`;
+  // if (activeEnv == "production") {
+  //   filters = `filter: { fields: { slug: { ne: null } , prefix: { ne: null } } }`;
+  // }
+  // const result = await graphql(
+  //   `
+  //       {
+  //         allMarkdownRemark(
+  //           ` +
+  //     filters +
+  //     `
+  //           sort: { fields: [fields___prefix], order: DESC }
+  //           limit: 1000
+  //         ) {
+  //           edges {
+  //             node {
+  //               id
+  //               fields {
+  //                 slug
+  //                 prefix
+  //                 source
+  //               }
+  //               frontmatter {
+  //                 title
+  //                 category
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     `
+  // );
+  // if (result.errors) {
+  //   console.log(result.errors);
+  //   throw new Error(result.errors);
+  // }
+  // const items = result.data.allMarkdownRemark.edges;
+  // // Create category list
+  // const categorySet = new Set();
+  // items.forEach(edge => {
+  //   const {
+  //     node: {
+  //       frontmatter: { category }
+  //     }
+  //   } = edge;
+  //   if (category && category !== null) {
+  //     categorySet.add(category);
+  //   }
+  // });
+  // // Create category pages
+  // const categoryList = Array.from(categorySet);
+  // categoryList.forEach(category => {
+  //   createPage({
+  //     path: `/category/${_.kebabCase(category)}/`,
+  //     component: categoryTemplate,
+  //     context: {
+  //       category
+  //     }
+  //   });
+  // });
+  // // Create posts
+  // const posts = items.filter(item => item.node.fields.source === "posts");
+  // posts.forEach(({ node }, index) => {
+  //   const slug = node.fields.slug;
+  //   const next = index === 0 ? undefined : posts[index - 1].node;
+  //   const prev = index === posts.length - 1 ? undefined : posts[index + 1].node;
+  //   const source = node.fields.source;
+  //   createPage({
+  //     path: slug,
+  //     component: postTemplate,
+  //     context: {
+  //       slug,
+  //       prev,
+  //       next,
+  //       source
+  //     }
+  //   });
+  // });
+  // // and pages.
+  // const pages = items.filter(item => item.node.fields.source === "pages");
+  // pages.forEach(({ node }) => {
+  //   const slug = node.fields.slug;
+  //   const source = node.fields.source;
+  //   createPage({
+  //     path: slug,
+  //     component: pageTemplate,
+  //     context: {
+  //       slug,
+  //       source
+  //     }
+  //   });
+  // });
 };
 
 // exports.onCreateWebpackConfig = ({ stage, actions }, options) => {
